5dcd1ac44b878f56474c9bcdb264f1196a205c7c
Jorge Luis Vasquez <jlv0362@gmail.com>
2025-08-11 18:04:07 -0300

    Primer Commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return """
+            <h1 style="font-size: 64px;color: blue;">Hola Jorge</h1>
+            """
+
+if __name__ == '__main__':
+    app.run(debug=True)
